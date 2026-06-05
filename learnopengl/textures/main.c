6d85b75cffb13f89871aafe64bda27ea30e250e3
demial003 <demial003@gmail.com>
2026-06-05 13:50:15 -0400

    textures default file

--- learnopengl/textures/main.c
+++ learnopengl/textures/main.c
@@ -1,1 +1,144 @@
-hello world
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *vertexShaderSource = "#version 400 core\n"
+                                 "layout (location = 0) in vec2 aPos;\n"
+                                 "layout (location = 1) in vec3 vecColor;\n"
+                                 "out vec3 theColor;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "  gl_Position = vec4(aPos, 0.0, 1.0);\n"
+                                 "  theColor = vecColor;\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 400 core\n"
+                                   "in vec3 theColor;\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "FragColor = vec4(theColor, 1.0);\n"
+                                   "}\0";
+
+unsigned int VBO;
+unsigned int VAO;
+unsigned int shaderProgram;
+unsigned int v_posLoc;
+unsigned int v_colorLoc;
+
+void setupGeometry() {
+  float vertices[] = {
+      -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, //
+      0.0f,  0.5f, 1.0f, 1.0f, 0.0f, //
+      0.5f,  0.0f, 1.0f, 1.0f, 0.0f, //
+  };
+
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(v_posLoc, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                        (void *)0);
+  glVertexAttribPointer(v_colorLoc, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                        (void *)(2 * sizeof(float)));
+  glEnableVertexAttribArray(v_posLoc);
+  glEnableVertexAttribArray(v_colorLoc);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+}
+
+void setupShaders() {
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    printf("ERROR SHADER FAILED %s\n", infoLog);
+  }
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    printf("ERROR SHADER FAILED %s\n", infoLog);
+  }
+
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    printf("Failed to compile shader program %s", infoLog);
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+int main(void) {
+  GLFWwindow *window;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+  window = glfwCreateWindow(900, 900, "textures", NULL, NULL);
+  if (window == NULL) {
+    fprintf(stderr, "failed to create glfw window");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    printf("failed to initialize GLAD");
+    exit(EXIT_FAILURE);
+  }
+
+  glViewport(0, 0, 600, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  setupShaders();
+  setupGeometry();
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
